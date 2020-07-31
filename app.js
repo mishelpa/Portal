77e0fa7da37bd7a4ef68f1078b69396654e9426a
mishelpa <judith.paredesartica@gmail.com>
2020-07-31 18:38:35 -0500

    Carrusel

--- app.js
+++ app.js
@@ -54,14 +54,17 @@ window.onload = () => {
     buttonNext.onclick = () => Move(2);
 
     function Move(value) {
-        const trackWidth = track.offsetWidth;
-        const listWidth = slickList.offsetWidth;
+        const trackWidth = track.offsetWidth; //976
+        const listWidth = slickList.offsetWidth; //198
         console.log(listWidth, trackWidth)
         track.style.left == "" ? leftPosition = track.style.left = 0 : leftPosition = parseFloat(track.style.left.slice(0, -2) * -1);
-
+        if(leftPosition > (trackWidth - listWidth)) {
+          leftPosition = -slickWidth;
+        }
+        console.log(leftPosition);
         if(leftPosition < (trackWidth - listWidth) && value == 2) {
             track.style.left = `${-1 * (leftPosition + slickWidth)}px`;
-        } else if(leftPosition > 0 && value == 1) {
+        } else if(leftPosition >= 0 && value == 1) {
             track.style.left = `${-1 * (leftPosition - slickWidth)}px`;
         }
     }
